5f637795f5e51ea727e9113f3bdca68d5987f355
Andrew Delarosa <delarosa1254@gmail.com>
2025-10-28 20:15:14 +0000

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#welcome"
   get "portfolio", to: "pages#portfolio"
+  get "about", to: "about#index"
 end
